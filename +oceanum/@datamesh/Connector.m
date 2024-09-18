f08174e54a211c92519dc580982ee71a16e1d983
dave <d.johnson@oceanum.science>
2024-09-18 15:40:24 +1200

    Create connector stub

--- +oceanum/@datamesh/Connector.m
+++ +oceanum/@datamesh/Connector.m
@@ -1,3 +1,13 @@
 classdef Connector
-
+  properties (Access=private)
+    _token % 
+  end
+  methods
+    function datamesh=Connector(token)
+      arguments
+        token = ''
+      end
+      datamesh._token=token
+      fprintf("Connector created")
+   end
 end
