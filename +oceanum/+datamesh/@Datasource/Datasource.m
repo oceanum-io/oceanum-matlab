1ed45f63f43b7bac314ed0845018a0d3db489449
VictorPlaysBadminton <162258396+VictorPlaysBadminton@users.noreply.github.com>
2025-11-25 15:32:35 +1300

    Fixed field assignments in Datasource

--- +oceanum/+datamesh/@Datasource/Datasource.m
+++ +oceanum/+datamesh/@Datasource/Datasource.m
@@ -1,4 +1,19 @@
 classdef Datasource < handle
+    % DATASOURCE - Representation of a data source with metadata and schema
+    %   DS = DATASOURCE() encapsulates metadata, spatial-temporal bounds,
+    %   schema and driver configuration for accessing dataset contents.
+    %
+    %   Data Fields:
+    %       coordinates, geom, geometry - spatial coordinate representations
+    %       tstart, tend, created, modified - temporal and provenance info
+    %       dataschema, attributes, variables - schema and variable info
+    %       driver, driver_args - backend driver configuration
+    %   Datasource Properties
+    %       attributes - Datasource global attributes. Note that these are None (undefined) for a summary dataset.
+    %       bounds - Bounding box of datasource geographical extent
+    %       geometry - geometry of datasource
+    %       variables - Datasource variables (or properties). Note that these are None (undefined) for a summary dataset.
+
     % Fields
     properties (Access = public)
         coordinates
@@ -27,68 +42,96 @@ classdef Datasource < handle
         attributes
         bounds
         geometry
+        variables
     end
     
     methods
         function obj = Datasource(props)
+          % DATASOURCE - Construct a Datasource object from a struct
+          %
+          % Input arguments:
+          % props - struct with optional fields used to populate properties
+          %
+          % This constructor copies known fields from props into the object.
             arguments
                 props struct
             end
             
-            % Set properties from input struct
-            if isfield(props, 'id')
-                obj.id = props.id;
+            % Set fields from input struct
+            if isfield(props, 'coordinates')
+                obj.coordinates = props.coordinates;
             end
-            if isfield(props, 'name')
-                obj.name = props.name;
+            if isfield(props, 'created')
+                obj.created = props.created;
+            end
+            if isfield(props, 'dataschema')
+                obj.dataschema = props.dataschema;
             end
             if isfield(props, 'description')
                 obj.description = props.description;
-            end
-            if isfield(props, 'geom')
-                obj.geom = props.geom;
+            end            
+            if isfield(props, 'details')
+                obj.details = props.details;
             end
             if isfield(props, 'driver')
                 obj.driver = props.driver;
             end
             if isfield(props, 'driver_args')
-                obj.driverArgs = props.driver_args;
+                obj.driver_args = props.driver_args;
             end
-            if isfield(props, 'variables')
-                obj.variables = props.variables;
+            if isfield(props, 'expires')
+                obj.expires = props.expires;
             end
-            if isfield(props, 'coordinates')
-                obj.coordinates = props.coordinates;
+            if isfield(props, 'geom')
+                obj.geom = props.geom;
             end
-            if isfield(props, 'crs')
-                obj.crs = props.crs;
+            if isfield(props, 'id')
+                obj.id = props.id;
+            end
+            if isfield(props, 'info')
+                obj.info = props.info;
+            end
+            if isfield(props, 'labels')
+                obj.labels = props.labels;
+            end
+            if isfield(props, 'modified')
+                obj.modified = props.modified;
+            end
+            if isfield(props, 'name')
+                obj.name = props.name;
+            end
+            if isfield(props, 'parameters')
+                obj.parameters = props.parameters;
+            end
+            if isfield(props, 'parchive')
+                obj.parchive = props.parchive;
+            end
+            if isfield(props, 'pforecast')
+                obj.pforecast = props.pforecast;
             end
             if isfield(props, 'tags')
                 obj.tags = props.tags;
             end
-            if isfield(props, 'metadata')
-                obj.metadata = props.metadata;
+            if isfield(props, 'tend')
+                obj.tend = props.tend;
             end
-            if isfield(props, 'created')
-                obj.created = props.created;
+            if isfield(props, 'tstart')
+                obj.tstart = props.tstart;
             end
-            if isfield(props, 'updated')
-                obj.updated = props.updated;
-            end
-            if isfield(props, 'size')
-                obj.size = props.size;
-            end
-            if isfield(props, 'dlen')
-                obj.dlen = props.dlen;
-            end
+
+            % Assign Properties 
+
+
         end
         
         function str = char(obj)
+          % CHAR - Create a human-readable summary of the datasource object
             str = sprintf('Datasource: %s [%s]', obj.name, obj.id);
             if ~isempty(obj.description)
                 str = sprintf('%s\nDescription: %s', str, obj.description);
             end
             if ~isempty(obj.variables)
+              % Format variables differently for cell arrays vs. other types
                 if iscell(obj.variables)
                     vars = strjoin(obj.variables, ', ');
                 else
@@ -96,12 +139,10 @@ classdef Datasource < handle
                 end
                 str = sprintf('%s\nVariables: %s', str, vars);
             end
-            if ~isempty(obj.size)
-                str = sprintf('%s\nSize: %d bytes', str, obj.size);
-            end
         end
         
         function disp(obj)
+          % DISP - Print the char representation with a trailing newline
             fprintf('%s\n', char(obj));
         end
         
